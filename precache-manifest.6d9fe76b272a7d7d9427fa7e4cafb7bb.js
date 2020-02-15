1790cb209ea8cc74a98f11563dbe936a8b81e703
Kyle Whittemore <kyle.whittemore@gmail.com>
2020-02-15 17:14:26 -0500

    Updates

---
+++ precache-manifest.6d9fe76b272a7d7d9427fa7e4cafb7bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc35fa0ccd3a8e039b4213de62a56527",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "c03aa41940296e1a8222",
+    "url": "/portfolio-v2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8377792e985d327309e6",
+    "url": "/portfolio-v2/static/js/2.93f835db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio-v2/static/js/2.93f835db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03aa41940296e1a8222",
+    "url": "/portfolio-v2/static/js/main.2654f22a.chunk.js"
+  },
+  {
+    "revision": "7d733817a1b183f367af",
+    "url": "/portfolio-v2/static/js/runtime-main.f5703142.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-v2/static/media/logo.5d5d9eef.svg"
+  }
+]);
