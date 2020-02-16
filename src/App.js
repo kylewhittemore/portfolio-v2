3dc4bece036a5cbb687662e387590919074e7c48
Kyle Whittemore <kyle.whittemore@gmail.com>
2020-02-15 20:47:01 -0500

    continued to scaffold.  Added pages and router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/home/index';
+import Blog from './pages/blog/index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path={process.env.PUBLIC_URL + '/'} render={props => <Home {...props}/>}/>
+        <Route exact path={process.env.PUBLIC_URL + '/blog'} render={props => <Blog {...props}/>} />
+      </Switch>
+    </Router>
+
   );
 }
 
---
+++ src/components/BlogPost/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function BlogPost() {
+    return (
+        <div><h1>blog page</h1></div>
+    )
+}
